323ee4cc6454a4886592b7efa68785c44b27812f
Ismael <swalehismael144@gmail.com>
2025-03-26 15:37:10 +0300

    Working on contact us page (pt 1)

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
-from .views import HomeView
+from django.urls import path
+from .views import HomeView, ContactUsView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')    
+    path("", HomeView.as_view(), name="home"),
+    path("contact-us/", ContactUsView.as_view(), name="contact-us"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import View 
+from django.views.generic import View
 
-class HomeView(View): 
-    def get(self, request): 
+
+class HomeView(View):
+    def get(self, request):
         return render(request, "index.html", {})
-    
-    
+
+
+class ContactUsView(View):
+    def get(self, request):
+        return render(request, "contact-us.html", {})
+
+    def post(self, request): 
+        return render(request, "contact-us.html", {})
+        
