8ee9b608e49f2b2d19018b5be403510859374659
Ismael <swalehismael144@gmail.com>
2025-03-26 16:01:30 +0300

    Added the EmailSender helper

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from helpers.email_manager import MessageBuilder, EmailManager
 
 
 class HomeView(View):
@@ -11,6 +12,9 @@ class ContactUsView(View):
     def get(self, request):
         return render(request, "contact-us.html", {})
 
-    def post(self, request): 
+    def post(self, request):
         return render(request, "contact-us.html", {})
-        
+
+    def send_email(self, message: dict[str, str]) -> bool:
+        # todo
+        ...
