9f63dd48c495acf15f3cc945f76337f992617873
Ismael <swalehismael144@gmail.com>
2025-03-26 15:45:05 +0300

    Created the ContactMessage model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model): 
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=30, null=True, blank=True)
+    message = models.TextField()
+    
+    def __str__(self): 
+        return f"Message from {self.name} about '{self.subject}'"
